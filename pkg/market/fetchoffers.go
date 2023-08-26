b08ccf5c11249a7eae1ed554e760ed8de50c2e2e
KΞkzploit <kekzploit@proton.me>
2023-08-26 23:56:31 +0100

    onion directory complete, renders string contains

---
+++ pkg/market/fetchoffers.go
@@ -0,0 +1,77 @@
+package market
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func GetOffersGlobalTx() {
+	jsonBody := fmt.Sprintln(`{
+		"jsonrpc": "2.0",
+		"id": "0",
+		"method": "marketplace_global_get_offers_ex",
+		"params": {
+			"filter": {
+				"amount_low_limit": 0,
+				"amount_up_limit": 0,
+				"bonus": false,
+				"category": "",
+				"fake": false,
+				"keyword": "",
+				"limit": 100,
+				"location_city": "",
+				"location_country": "",
+				"offer_type_mask": 0,
+				"offset": 0,
+				"order_by": 0,
+				"primary": "",
+				"rate_low_limit": "0.000000",
+				"rate_up_limit": "0.000000",
+				"reverse": false,
+				"target": "",
+				"timestamp_start": 0,
+				"timestamp_stop": 0
+			}
+		}
+	}`)
+
+	request, err := http.NewRequest("POST", daemonUrl, bytes.NewBuffer([]byte(jsonBody)))
+	if err != nil {
+		fmt.Println("error") // return meaningful statement
+	}
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+	client := &http.Client{}
+	res, err := client.Do(request)
+	if err != nil {
+		fmt.Println("error") // return meaningful statement
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Println("error")
+		}
+	}(res.Body)
+
+	body, _ := io.ReadAll(res.Body)
+	data := Offers{}
+
+	_ = json.Unmarshal([]byte(body), &data)
+
+	currentVendor := fmt.Sprintf("vendor:%s", alias)
+
+	for _, offer := range data.Result.Offers {
+		if currentVendor == offer.Cnt {
+			return true
+		}
+	}
+
+	return false
+}
+
+func GetAllOffers() bool {
+	return true
+}
