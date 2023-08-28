26aec8dcb369cb83e7dc131f3ff31f57eef77f1f
KΞkzploit <kekzploit@proton.me>
2023-08-28 15:48:46 +0100

    building offer management

--- frontend/src/components/ManageOffers.jsx
+++ frontend/src/components/ManageOffers.jsx
@@ -31,14 +31,48 @@ export const ManageOffers = () => {
                 <h2 className="text-2xl">Offer management</h2>
             </div>
 
-            <div className="grid grid-cols-2 gap-5 ">
+            <div className="grid grid-cols-2 gap-10 ">
                 <div className="dash-card rounded-lg shadow-lg">
-                    <h1 className="text-2xl mb-5">✅ Create offer</h1>
-                    <p className="text-xl mb-5">
-                        Create and post a new offer to the Zano Blockchain now.
-                    </p>
+                    <div className="grid mb-3">
+                        <div className="grid">
+                            <h1 className="text-2xl mb-5">✅ Create offer</h1>
+                            <p className="text-xl mb-5 create">
+                                Create and post a new offer to the Zano
+                                Blockchain now.
+                            </p>
+                        </div>
+                        <div className="grid grid-cols-2 gap-2 overflow-auto text-black create">
+                            <div className="mb-1">
+                                <span className="text-sm">Title</span>
+                                <input className="p-0.5 w-full" type="text" />
+                            </div>
+                            <div className="mb-1">
+                                <span className="text-sm">Description</span>
+                                <input className="p-0.5 w-full" type="text" />
+                            </div>
+                            <div className="mb-1">
+                                <span className="text-sm">Image URL</span>
+                                <input className="p-0.5 w-full" type="text" />
+                            </div>
+                            <div className="mb-1">
+                                <span className="text-sm">
+                                    Accepted payments
+                                </span>
+                                <input className="p-0.5 w-full" type="text" />
+                            </div>
+                            <div className="mb-1">
+                                <span className="text-sm">Ships to</span>
+                                <input className="p-0.5 w-full" type="text" />
+                            </div>
+                            <div className="mb-1">
+                                <span className="text-sm">Amount</span>
+                                <input className="p-0.5 w-full" type="text" />
+                            </div>
+                        </div>
+                    </div>
 
-                    <div className="grid grid-cols-2 mt-5 gap-3">
+                    {/*  */}
+                    <div className="grid grid-cols-2 gap-4">
                         <button
                             className="rounded bg-purple-700 mb-3 hover:bg-purple-600 active:bg-purple-500 text-white p-2"
                             onClick={() => {
@@ -84,7 +118,7 @@ export const ManageOffers = () => {
                         slidesPerView={1}
                         centeredSlides={true}
                         autoplay={{
-                            delay: 10000,
+                            delay: 4000,
                             disableOnInteraction: false,
                         }}
                         pagination={{
@@ -95,12 +129,18 @@ export const ManageOffers = () => {
                         className=""
                     >
                         <SwiperSlide className="mb-14 grid grid-rows-3 rounded-lg p-3 border-2 border-purple-700">
-                            <div className="mb-2">
+                            <div className="grid grid-cols-2 mb-2 justify-left items-center align-middle text-center">
+                                <img
+                                    src="https://assets.survivornet.com/wp-content/uploads/2019/05/30184931/psychedelic-mushrooms.jpg"
+                                    alt=""
+                                    width={250}
+                                    className="rounded-lg"
+                                />
                                 <h3 className="text-3xl text-purple-400">
-                                    Title
+                                    1oz of Golden Caps
                                 </h3>
                             </div>
-                            <div className="mb-2">
+                            <div className="mb-4">
                                 <p className="text-md">
                                     Lorem ipsum dolor sit, amet consectetur
                                     adipisicing elit. Repellat, ipsum corrupti!
@@ -109,8 +149,96 @@ export const ManageOffers = () => {
                                 </p>
                             </div>
                             <div className="">
-                                <p className="text-cm">
-                                    Hash: khgjhgsdfjg4y5gj4hg5j45hgj4g5
+                                <p className="text-sm text-purple-400">
+                                    TxHash: khgjhgsdfjg4y5gj4hg5j45hgj4g5
+                                </p>
+                            </div>
+                            <div className="grid grid-cols-2 mt-5 gap-3">
+                                <button
+                                    className="rounded bg-purple-700 mb-3 hover:bg-purple-600 active:bg-purple-500 text-white p-2"
+                                    onClick={() => {
+                                        alert("doin bits");
+                                    }}
+                                >
+                                    Update
+                                </button>
+                                <button
+                                    className="rounded bg-purple-700 mb-3 hover:bg-purple-600 active:bg-purple-500 text-white p-2"
+                                    onClick={() => {
+                                        alert("doin bits");
+                                    }}
+                                >
+                                    Cancel
+                                </button>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide className="mb-14 grid grid-rows-3 rounded-lg p-3 border-2 border-purple-700">
+                            <div className="grid grid-cols-2 mb-2 justify-left items-center align-middle text-center">
+                                <img
+                                    src="https://assets.survivornet.com/wp-content/uploads/2019/05/30184931/psychedelic-mushrooms.jpg"
+                                    alt=""
+                                    width={250}
+                                    className="rounded-lg"
+                                />
+                                <h3 className="text-3xl text-purple-400">
+                                    1oz of Golden Caps
+                                </h3>
+                            </div>
+                            <div className="mb-4">
+                                <p className="text-md">
+                                    Lorem ipsum dolor sit, amet consectetur
+                                    adipisicing elit. Repellat, ipsum corrupti!
+                                    Sed temporibus, fugit non adipisci soluta
+                                    exercitationem impedit dolor!
+                                </p>
+                            </div>
+                            <div className="">
+                                <p className="text-sm text-purple-400">
+                                    TxHash: khgjhgsdfjg4y5gj4hg5j45hgj4g5
+                                </p>
+                            </div>
+                            <div className="grid grid-cols-2 mt-5 gap-3">
+                                <button
+                                    className="rounded bg-purple-700 mb-3 hover:bg-purple-600 active:bg-purple-500 text-white p-2"
+                                    onClick={() => {
+                                        alert("doin bits");
+                                    }}
+                                >
+                                    Update
+                                </button>
+                                <button
+                                    className="rounded bg-purple-700 mb-3 hover:bg-purple-600 active:bg-purple-500 text-white p-2"
+                                    onClick={() => {
+                                        alert("doin bits");
+                                    }}
+                                >
+                                    Cancel
+                                </button>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide className="mb-14 grid grid-rows-3 rounded-lg p-3 border-2 border-purple-700">
+                            <div className="grid grid-cols-2 mb-2 justify-left items-center align-middle text-center">
+                                <img
+                                    src="https://assets.survivornet.com/wp-content/uploads/2019/05/30184931/psychedelic-mushrooms.jpg"
+                                    alt=""
+                                    width={250}
+                                    className="rounded-lg"
+                                />
+                                <h3 className="text-3xl text-purple-400">
+                                    1oz of Golden Caps
+                                </h3>
+                            </div>
+                            <div className="mb-4">
+                                <p className="text-md">
+                                    Lorem ipsum dolor sit, amet consectetur
+                                    adipisicing elit. Repellat, ipsum corrupti!
+                                    Sed temporibus, fugit non adipisci soluta
+                                    exercitationem impedit dolor!
+                                </p>
+                            </div>
+                            <div className="">
+                                <p className="text-sm text-purple-400">
+                                    TxHash: khgjhgsdfjg4y5gj4hg5j45hgj4g5
                                 </p>
                             </div>
                             <div className="grid grid-cols-2 mt-5 gap-3">
--- frontend/src/components/WalletData.jsx
+++ frontend/src/components/WalletData.jsx
@@ -8,7 +8,7 @@ export const WalletData = () => {
     return (
         <div className="flex flex-col">
             {/* main container */}
-            <div className="grid grid-cols-3 gap-5 mt-20">
+            <div className="grid grid-cols-3 gap-10 mt-20">
                 <div className="dash-card rounded-lg shadow-lg">
                     <h1 className="text-2xl mb-1">
                         Balance{" "}
