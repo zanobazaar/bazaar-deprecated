23020f784dad832ab87e9e465cef22a5fd26dce6
KΞkzploit <kekzploit@proton.me>
2023-08-31 00:27:04 +0100

    added sell alias form to dashboard

--- frontend/src/components/AliasMarket.jsx
+++ frontend/src/components/AliasMarket.jsx
@@ -6,6 +6,11 @@ import AliasImg from "../assets/images/alias_marketplace.png";
 
 export const AliasMarket = () => {
     const {} = useContext(DefaultContext);
+
+    const [aliasContact, setAliasContact] = useState(
+        "https://discord.gg/FHjK3SEC"
+    );
+
     return (
         <div className="flex flex-col">
             {/* main container */}
@@ -13,24 +18,47 @@ export const AliasMarket = () => {
                 <h2 className="text-2xl">Alias listing</h2>
             </div>
 
-            <div className="dash-card shadow-lg grid grid-cols-3 gap-10 justify-center align-middle items-center">
+            <div className="dash-card create shadow-lg grid grid-cols-2 gap-10 justify-center align-middle items-center">
                 <div>
                     <img
-                        className="rounded-lg"
+                        className="rounded-lg mb-3"
                         src={AliasImg}
-                        height={200}
+                        height={500}
                         alt=""
                     />
                 </div>
 
-                <div>
-                    <h3 className="text-sky-400 text-2xl">List your Alias</h3>
-                    <p className="text-xl">
-                        Zano Bazaar introduces the Alias Marketplace, buy and
-                        sell alias's now.
-                    </p>
-                </div>
                 <div className="grid grid-cols-1 mt-5 gap-3">
+                    <div>
+                        <h3 className="text-sky-400 text-3xl">
+                            List your Alias
+                        </h3>
+                        <p className="text-xl">
+                            Zano Bazaar introduces the Alias Marketplace, buy
+                            and sell alias's now.
+                        </p>
+                    </div>
+                    <div className="mb-1">
+                        <span className="text-sm">Description</span>
+                        <input
+                            className="p-0.5 w-full"
+                            type="text"
+                            value={""}
+                            onChange={(event) => {}}
+                        />
+                    </div>
+                    <div className="mb-1">
+                        <span className="text-sm">Where to find me</span>
+                        <input
+                            className="p-0.5 w-full"
+                            type="text"
+                            placeholder="https://discord.gg/FHjK3SEC"
+                            value={aliasContact}
+                            onChange={(event) => {
+                                setAliasContact(event.target.value);
+                            }}
+                        />
+                    </div>
                     <button
                         className="rounded bg-purple-700 mb-3 hover:bg-purple-600 active:bg-purple-500 text-white p-2"
                         onClick={() => {
